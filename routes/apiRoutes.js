0b27ad00e9e046727a7b56f04404a4dd7d43fa39
Leigh Johnson <leigh.carterjohnson@gmail.com>
2021-08-14 01:12:01 -0700

    finally fixed the route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,41 @@
 const fs = require('fs');
-const note = require('../db/db.json');
+const noteData = require('../db/db.json');
 const uniqid = require('uniqid');
+const db = 'db/db.json';
 
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
         let db = JSON.parse(fs.readFileSync('./db/db.json'));
-        res.JSON(db);
+        res.json(db);
     });
 
     app.post('/api/notes', (req, res) => {
         let notes = JSON.parse(fs.readFileSync('db/db.json'));
-        res.JSON(notes);
+        res.json(notes);
 
-        const newNote = {
+        const newNotes = {
             title: req.body.title,
             text: req.body.text,
             id: uniqid(),
         };
 
-        nootz.push(newNote);
+        notes.push(newNotes);
         fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-        res.JSON(notes);
+        res.json(notes);
     });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const {id} = req.params;
+        let notes = fs.readFileSync(db)
+        notes = JSON.parse(notes);
+        let indexToDelete = notes.filter((each) => each.id !=id);
+
+        if(!indexToDelete) {
+            return res.status(404).json({error: 'note does not exist'});
+        }
+
+        fs.writeFileSync(db, JSON.stringify(indexToDelete));
+            res.send(db, indexToDelete);
+        });
 };
